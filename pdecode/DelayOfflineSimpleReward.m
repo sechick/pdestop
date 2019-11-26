eaacf608a7ab27e29a3891de6b5fcb3b8b8c77d1
sechick <sechick@alumni.stanford.edu>
2019-11-26 17:37:02 +0100

    fix of delay constant tau

--- pdecode/DelayOfflineSimpleReward.m
+++ pdecode/DelayOfflineSimpleReward.m
@@ -20,7 +20,12 @@ if 0 % commented out: we need to handle P in the 'pdescalestandardize' and the '
     end
 end
 
-    sincrem = s0 * (tau * scale.gamma * s0) / (1 + tau * scale.gamma * s0); % find ds in going from t=1/(gamma s0) to t+tau, in s coords
+    timet = 1 / (scale.gamma * s0);                 % get time t equivalent of the time to test, s0.
+    uinpipe = max(0, min(tau, timet - param.t0));   % compute number of patients in pipeline. 
+    suinpipe = 1 / (scale.gamma * (timet + uinpipe)); % compute time when decision is made after pipeline patients seen, 
+    
+    sincrem = abs( suinpipe - s0);      % want to compute time accounting for fact num pipeline might be less than tau if in 'stage I' sampling
+%    sincrem = s0 * (tau * scale.gamma * s0) / (1 + tau * scale.gamma * s0); % find ds in going from t=1/(gamma s0) to t+tau, in s coords
 
     %dincrem = sqrt(3*sincrem/2);    % implied reward from diffusion over that increment
     evivec = max(0, wvec); 
